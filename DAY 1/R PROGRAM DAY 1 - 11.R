6541e8580b2d46e06e03e6cbd852d90703fb9c5c
192121009 Swetha <112751881+swethasures@users.noreply.github.com>
2023-05-04 19:05:02 +0530

    Add files via upload

---
+++ DAY 1/R PROGRAM DAY 1 - 11.R
@@ -0,0 +1,10 @@
+str1 = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+print("Original vector(string)")
+print(str1)
+print("Unique elements of the said vector:")
+print(unique(tolower(str1)))
+nums = c(1, 2, 2, 3, 4, 4, 5, 6)
+print("Original vector(number)")
+print(nums)
+print("Unique elements of the said vector:")
+print(unique(nums))
